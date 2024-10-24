b7ade231afa3bdd85832975c973801fb023eae35
HugoLoud <162356902+HugoLoud@users.noreply.github.com>
2024-10-25 00:57:15 +0300

    Update cartesian_product.py

--- project/cartesian_product.py
+++ project/cartesian_product.py
@@ -13,10 +13,10 @@ def cartesian_product_sum(lists: List[List[int]]) -> int:
 
     Args:
         lists (List[List[int]]): A list of integer lists for which the Cartesian product will be computed.
-    
+
     Returns:
         int: The total sum of all elements in the Cartesian product.
-    
+
     Example:
         cartesian_product_sum([[1, 2], [3, 4]]) will compute the Cartesian product:
         (1,3), (1,4), (2,3), (2,4) and return 20, because (1+3) + (1+4) + (2+3) + (2+4) = 20.
